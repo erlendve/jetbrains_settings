0740a982112a4a72cb5a1c1fb5fdfeaaf3acdfd7
verle <Erlend.Larsen.Vestad2@helsedir.no>
2017-09-13 11:08:19 +0200

    WS-2017.2.3 <admverle@TVRA-WIN-FRIDA3 Create baseRefactoring.xml, ide.general.xml, git.xml, project.default.xml, Default copy.xml, other.xml, Default.xml, applicationLibraries.xml, Monokai Sublime Text 3.icls, file.template.settings.xml, find.xml, ui.lnf.xml, keymap.xml, colors.scheme.xml, notifications.xml, React Test Suite༏ext༏.test.jsx, Ladies Night 2.icls, ReactJS.xml, emmet.xml, React Stateless Component File.jsx, Default.xml, laf.xml, vcs.xml, editor.xml, debugger.xml

---
+++ fileTemplates/React Stateless Component File.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function ${NAME}(props) {
+    return (
+        <div></div>
+    );
+}
+
+${NAME}.propTypes = {};
+${NAME}.defaultProps = {};
+
+export default ${NAME};
---
+++ fileTemplates/React Test Suite༏ext༏.test.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {shallow} from 'enzyme';
+
+import ${NAME} from '../../src/$path/${NAME}';
+
+describe('${NAME}', () => {
+    it('should', () => {
+        const wrapper = shallow(<${NAME}/>);
+    });
+});
